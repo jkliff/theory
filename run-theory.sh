f4f5162372c4e5d0deaa00d835204252e65f8797
john <john@bataille.(none)>
2012-05-05 18:01:23 +0200

    separate reload from start

--- run-theory.sh
+++ run-theory.sh
@@ -11,8 +11,19 @@ start() {
 		exit
 	fi
 	echo "starting theory"
-	"$SELF/env/bin/paster" serve --daemon --reload --log-file "$SELF/theory.log" "$SELF/server.ini" && echo "theory started, http://localhost:9099/ if you're using the default port"
+	"$SELF/env/bin/paster" serve --daemon --log-file "$SELF/theory.log" "$SELF/server.ini" && echo "theory started, http://localhost:9099/ if you're using the default port"
 }
+
+restart() {
+	if [ ! -e "$SELF/env/bin/paster" ]
+	then
+		echo "please install the prerequisites by using the included install.sh script"
+		exit
+	fi
+	echo "restarting theory"
+	"$SELF/env/bin/paster" serve --reload --log-file "$SELF/theory.log" "$SELF/server.ini" && echo "theory started, http://localhost:9099/ if you're using the default port"
+}
+
 
 stop() {
 	echo "stopping theory"
@@ -26,8 +37,11 @@ case $1 in
 'stop')
 	stop;
 	;;
+'reload')
+    reload;
+    ;;
 *)
-	echo "usage: run-theory (start|stop)"
+	echo "usage: run-theory (start|stop|reload)"
 	;;
 esac
 
